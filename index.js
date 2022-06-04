856f58035916c8c6436440439e5ef47c4d8b5c8a
Vincent Langat <kimutailangat033@gmail.com>
2022-06-04 12:57:06 +0300

    commit tree 23ff1584fe3ea8562c8a529e6824d4f54a79f0ee

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 const main = document.querySelector("main");
 main.remove();
 const newHeader= document.createElement('h1')
- newHeader.id="victory";
+ newHeader.id="victory"; //or the code below
+ //newHeader.setAttribute("id", "victory"); 
  newHeader.innerHTML="Vinnie Kim is the champion";
